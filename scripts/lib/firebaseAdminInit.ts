552286d457f4e9050f0bae951ca4a155334804d7
mft-collab <muftum@gmail.com>
2026-04-20 11:59:53 +0300

    Sync scripts/lib/firebaseAdminInit.ts

---
+++ scripts/lib/firebaseAdminInit.ts
@@ -0,0 +1,11 @@
+import admin from 'firebase-admin';
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+  });
+}
+
+export const db = admin.firestore();
+export const FieldValue = admin.firestore.FieldValue;
+export const Timestamp = admin.firestore.Timestamp;
